84fef7fd00a3f6455a4fbcb0eb680f29ba8f89df
Sergey V <sftp.mtuci@gmail.com>
2016-04-03 21:55:04 +0300

    add loadavg

---
+++ usr/lib/graphite-sender/loadavg.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use v5.10;
+
+my $file = '/proc/loadavg';
+
+open(FD, $file) || die "Can't open '$file': $!\n";
+
+while (<FD>) {
+    if ($_ =~ m!(\d+\.\d+) (\d+\.\d+) (\d+\.\d+) (\d+)/(\d+)!) {
+	say "la1 $1";
+	say "la5 $2";
+	say "la15 $3";
+	say "run_proc $4";
+	say "total_proc $5";
+    }
+}
+
+close(FD);
