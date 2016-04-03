f7b7b11924534b563fbbef07dd385bc739222ac2
Sergey V <sftp.mtuci@gmail.com>
2016-04-03 22:00:19 +0300

    add meminfo

---
+++ usr/lib/graphite-sender/meminfo.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use v5.10;
+
+my $file = '/proc/meminfo';
+
+open(FD, $file) || die "Can't open '$file': $!\n";
+
+while (<FD>) {
+    if ($_ =~ m/([\w\d]+):\s+(\d+)\skB/) {
+	say "$1 " . $2 * 1024;
+    }
+}
+
+close(FD);
